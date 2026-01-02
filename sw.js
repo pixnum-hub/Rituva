e6b2389faaf7910786041a8601df388c512bf14f
Manik Roy <pixnum@gmail.com>
2026-01-02 07:55:07 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "rituva-cache-v1";
+const urlsToCache = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("activate", event => { event.waitUntil(self.clients.claim()); });
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));
+});
