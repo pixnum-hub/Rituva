c508e94e301a272d7f41db3341a49e9be8629d92
Manik Roy <pixnum@gmail.com>
2026-01-13 06:43:01 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,30 @@
-const CACHE="rituva-v1";
+const CACHE_NAME = "rituva-cache-v1";
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(c=>c.addAll([
-"./",
-"./index.html",
-"./manifest.json"
-]))
-);
-self.skipWaiting();
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ])
+    )
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-);
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
