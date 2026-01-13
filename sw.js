227fb1cc0cfc82000245a7a88414375b50630f02
Manik Roy <pixnum@gmail.com>
2026-01-13 19:25:22 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,45 @@
-const CACHE_NAME = "rituva-v2";
+const CACHE_NAME = 'rituva-cache-v1';
 const urlsToCache = [
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "https://unpkg.com/leaflet/dist/leaflet.css",
-  "https://unpkg.com/leaflet/dist/leaflet.js",
-  "https://unpkg.com/leaflet.heat/dist/leaflet-heat.js"
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://unpkg.com/leaflet/dist/leaflet.css',
+  'https://unpkg.com/leaflet/dist/leaflet.js',
+  'https://unpkg.com/leaflet.heat/dist/leaflet-heat.js',
+  'https://unpkg.com/leaflet-velocity/dist/leaflet-velocity.min.js'
 ];
 
-// INSTALL SW
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// ACTIVATE SW
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
-  );
-  self.clients.claim();
-});
-
-// FETCH HANDLER: Cache first, network fallback
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request).then(response => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      }).catch(()=>caches.match("./index.html"));
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
     })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request).then(fetchResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          if(event.request.url.startsWith('http')){
+            cache.put(event.request, fetchResponse.clone());
+          }
+          return fetchResponse;
+        });
+      }))
+      .catch(() => caches.match('/index.html'))
   );
 });
