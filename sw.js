3ab9a7f778580660e1f6cdf997d8c7251c35c476
Manik Roy <pixnum@gmail.com>
2026-01-13 18:30:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,48 @@
-const cacheName="rituva-v3";
-const assets=["/","/index.html","/manifest.json"];
+const CACHE_NAME = "rituva-v1";
+const urlsToCache = [
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "https://unpkg.com/leaflet/dist/leaflet.css",
+  "https://unpkg.com/leaflet/dist/leaflet.js",
+  "https://unpkg.com/leaflet.heat/dist/leaflet-heat.js"
+];
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(cacheName).then(c=>c.addAll(assets))
-);
+// INSTALL SERVICE WORKER AND CACHE FILES
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request)
-.then(r=>r||fetch(e.request))
-);
+// ACTIVATE SERVICE WORKER
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH FROM CACHE FIRST
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request).then(response => {
+        // Cache new requests
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(()=>caches.match("./index.html"));
+    })
+  );
 });
