378c446c52a3cb599b3fe6c729f9fc055fedc77c
Manik Roy <pixnum@gmail.com>
2026-01-21 21:03:09 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-const CACHE = "rituva-v5";
+const CACHE="rituva-v1";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll([
-      "./",
-      "./index.html",
-      "./manifest.json"
-    ]))
-  );
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener("activate", e=>{
+self.addEventListener("activate",e=>{
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-    ))
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))
   );
   self.clients.claim();
 });
-
-self.addEventListener("fetch", e=>{
-  if(e.request.method!=="GET")return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||
+      fetch(e.request).then(n=>{
+        if(e.request.url.startsWith("http")){
+          const c=n.clone();
+          caches.open(CACHE).then(cache=>cache.put(e.request,c));
+        }
+        return n;
+      }).catch(()=>r)
+    )
+  );
 });
